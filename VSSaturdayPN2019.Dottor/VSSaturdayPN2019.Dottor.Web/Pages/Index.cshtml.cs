451eaddd1d45f96d05d9cafc4ab94d731e5a04e1
Andrea Dottor <andrea@dottor.net>
2019-11-15 21:21:12 +0100

    aggiunta animazione e colore icone

--- VSSaturdayPN2019.Dottor/VSSaturdayPN2019.Dottor.Web/Pages/Index.cshtml.cs
+++ VSSaturdayPN2019.Dottor/VSSaturdayPN2019.Dottor.Web/Pages/Index.cshtml.cs
@@ -17,9 +17,9 @@ namespace VSSaturdayPN2019.Dottor.Web.Pages
             _logger = logger;
         }
 
-        public void OnGet()
+        public IActionResult OnGet()
         {
-
+            return RedirectToPage("Files");
         }
     }
 }
